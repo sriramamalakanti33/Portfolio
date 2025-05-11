4fcfbec2bc55df9eb71b437e5c940200923ff8b2
RAM <sriramamalakanti369@gmail.com>
2025-05-11 22:42:14 +0530

    Updated

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -29,9 +29,11 @@ function About() {
         <div className="experience">
           <h3>Experience:</h3>
           <ul>
+            <li>💼 <strong>Agile Methodology Virtual Experience Program</strong> - Cognizant (December 2022)</li>
             <li>💼 <strong>Web Developer Intern</strong> - Raj Reddy Center for Technology and Society (RCTS), IIIT Hyderabad (2023 - 2024)</li>
             <li>💼 <strong>AI/ML Virtual Internship</strong> - India Edu Program and Google for Developers (Jan 2024 - March 2024)</li>
             <li>💼 <strong>Andorid Developer Virtual Internship</strong> - India Edu Program and Google for Developers (April 2024 - June 2024)</li>
+            <li>💼 <strong>Zero Trust Cloud Security Virtual Internship</strong> - Zscaler (October 2024 - December 2024)</li>
           </ul>
         </div>
         {/* Education Section */}
@@ -49,17 +51,17 @@ function About() {
         <h3>Certifications</h3>
         <ul>
           <li>✅ Data Analysis with Python - [IBM], [2022]</li>
+          <li>✅ Introduction to Python - [IBM], [2022]</li>
           <li>✅ Communication and amp; Network Security - [Infosys], [2023]</li>
-          <li>✅ Fundamentals of Java Programming - [Board Infinity], [2024]</li>
-          <li>✅ Cloud Security Fundamentals - [Paloalto Networks], [2024]</li>
-          <li>✅ Cybersecurity Fundamentals - [Paloalto Networks], [2024]</li>
           <li>✅ Fundamentals of Information Security - [Infosys], [2023]</li>
           <li>✅ Fundamentals of Cryptography - [Infosys], [2023]</li>
           <li>✅ Identity and Access Management IAM - [Infosys], [2023]</li>
           <li>✅ Introduction to Cyber Security - [Infosys], [2023]</li>
-          <li>✅ Introduction to Python - [IBM], [2022]</li>
           <li>✅ Java SE 11 Programmer II: Secure Coding in Java SE 11 Applications - [Infosys], [2023]</li>
           <li>✅ Networking and Web Technology - [Infosys], [2023]</li>
+          <li>✅ Fundamentals of Java Programming - [Board Infinity], [2024]</li>
+          <li>✅ Cloud Security Fundamentals - [Paloalto Networks], [2024]</li>
+          <li>✅ Cybersecurity Fundamentals - [Paloalto Networks], [2024]</li>
         </ul>
       </div>
       <h3>Fun Fact:</h3>
